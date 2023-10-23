d11bca07a0cf957720931b9f176dc27218961c31
federicogelsi <fgelsi97@gmail.com>
2023-10-22 21:48:33 -0300

    Se elimino un componente sin uso

--- src/app/activities/quiz/QuizHome.jsx
+++ src/app/activities/quiz/QuizHome.jsx
@@ -1,13 +1,11 @@
 import React from "react";
 import BannerSuperior from "../../components/BannerSuperior";
 import EncuestaBox from "../../components/EncuestaBox";
-import Espacio from "../../components/Espacio";
 
 function QuizHome() {
   return (
     <div>
       <BannerSuperior />
-      <Espacio />
       <EncuestaBox />
     </div>
   );
