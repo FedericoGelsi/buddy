95a092834ddc13ce13b5566cb3ae57453aacf7f0
federicogelsi <fgelsi97@gmail.com>
2023-10-27 00:01:18 -0300

    se agrego delete y put

--- src/app/api/api.js
+++ src/app/api/api.js
@@ -17,3 +17,27 @@ export async function post(uri, data) {
 export async function get(uri) {
   return await fetch(`${buddyApiUrl}${uri}`);
 }
+
+export async function remove(uri) {
+  const res = await fetch(`${buddyApiUrl}${uri}`, {
+    method: "DELETE",
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+  });
+  return res;
+}
+
+export async function put(uri, data) {
+  const res = await fetch(`${buddyApiUrl}${uri}`, {
+    method: "PUT",
+    mode: "cors", // no-cors, *cors, same-origin
+    cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+    credentials: "same-origin", // include, *same-origin, omit
+    headers: {
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(data),
+  });
+  return res;
+}
