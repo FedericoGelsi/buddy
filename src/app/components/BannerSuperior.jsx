b9e964ba1fb61fdb7e1b0deab6201c4b70265eff
federicogelsi <fgelsi97@gmail.com>
2023-10-31 22:06:09 -0300

    Ahora el banner superior usa el logo en SVG

--- src/app/components/BannerSuperior.jsx
+++ src/app/components/BannerSuperior.jsx
@@ -1,8 +1,7 @@
 "use client";
 
 import { useState } from "react";
-import Image from "next/image";
-import BuddyLogo from "../assets/Logo.png";
+import { BuddyLogo } from "../assets/BuddyLogo";
 import { Avatar } from "@nextui-org/avatar";
 
 export default function BannerSuperior() {
@@ -10,7 +9,7 @@ export default function BannerSuperior() {
 
   return (
     <div className="w-full h-24 rounded-3xl flex justify-between items-center px-8 mb-4 bg-T500">
-      <Image src={BuddyLogo} alt="NextUI hero Image" />
+      <BuddyLogo />
       <div className="rounded-full flex justify-between items-center pl-6 gap-4 bg-T600">
         <div className="text-light">{nombre}</div>
         <Avatar
