3c3792a2b98418f30ac5bd7dac4ea4288b226b3c
Francisco Arenas <francisco.arenas@mercadolibre.com>
2023-11-01 01:35:13 -0300

    deleted old code inpercentage chart big

--- src/app/progress/PercentageChartBig.jsx
+++ src/app/progress/PercentageChartBig.jsx
@@ -17,16 +17,6 @@ export default function PercentageChartBig(props) {
                 <div style={{alignSelf: 'stretch', color: '#2C2C2E', fontSize: 16, fontFamily: 'Poppins', fontWeight: '500', wordWrap: 'break-word'}}>{props.title}</div>
             </div>
             <div style={{width: 192, height: 192, position: 'relative'}}>
-                
-                {/* <div style={{width: 192, height: 192, left: 0, top: 0, position: 'absolute', background: 'white', borderRadius: 9999}} />
-                <div style={{width: 192, height: 192, left: 0, top: 0, position: 'absolute', background: '#EBEBEC', boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)', borderRadius: 9999}} />
-                <div style={{width: 192, height: 192, left: 0, top: 0, position: 'absolute', background: '#AFD778', boxShadow: '0px 0px 4px rgba(0, 0, 0, 0.25)', borderRadius: 9999}} />
-                <div style={{width: 70, height: 62, left: 61, top: 65, position: 'absolute', justifyContent: 'flex-start', alignItems: 'flex-end', display: 'inline-flex'}}>
-                    <div style={{color: '#2C2C2E', fontSize: 48, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>{props.value}</div>
-                    <div style={{paddingBottom: 12, justifyContent: 'flex-start', alignItems: 'flex-end', gap: 10, display: 'flex'}}>
-                        <div style={{color: '#404042', fontSize: 20, fontFamily: 'Poppins', fontWeight: '600', wordWrap: 'break-word'}}>%</div>
-                    </div>
-                </div> */}
                 <Pie percentage={props.value} color={chartColor} size={2} unit="%" />
             </div>
         </div>
